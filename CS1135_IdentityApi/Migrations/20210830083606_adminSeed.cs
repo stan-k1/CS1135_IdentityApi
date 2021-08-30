f3f12fdd392cffd2a1c9485d8c57dec851b9e04a
Stan <sk2114@outlook.com>
2021-08-30 16:18:56 +0300

    Added Authentication

---
+++ CS1135_IdentityApi/Migrations/20210830083606_adminSeed.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CS1135_IdentityApi.Migrations
+{
+    public partial class adminSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e789", "56f1a308-e9d2-469b-b48f-bb69002825ef", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "ConcurrencyStamp", "Email", "EmailConfirmed", "FirstName", "IsActive", "LastName", "LockoutEnabled", "LockoutEnd", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e575", 0, "b84fb37f-6e2d-4959-949a-d29df7c778a8", "scrooge@mcduck.com", true, "Scrooge", true, "McDuck", false, null, "SCROOGE@MDCUCK.COM", "ADMIN", "AQAAAAEAACcQAAAAEDltPwnRfMy3sTMm5Gn7yCvQToADBviXVXVEANY9KD5YMIP1Gdsqf8SXomwdaFSmwg==", null, false, "", false, "admin" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e789", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "a18be9c0-aa65-4af8-bd17-00bd9344e789", "a18be9c0-aa65-4af8-bd17-00bd9344e575" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e789");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "a18be9c0-aa65-4af8-bd17-00bd9344e575");
+        }
+    }
+}
